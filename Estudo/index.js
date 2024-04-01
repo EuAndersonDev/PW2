697eafb35cd3fc9c1eda7ba98e9114e15c902c2a
Fabíola Reis <andersonaugustorei1227@gmail.com>
2024-04-01 19:27:51 -0300

    ...

--- Estudo/index.js
+++ Estudo/index.js
@@ -1,24 +1,30 @@
 const express = require("express");
 const app = express();
 
-app.get("/", function(req, res){
-    res.send("Seja bem vindo")
+app.get("/", function (req, res) {
+  res.sendFile(__dirname+"/index.html");
 });
 
-app.get("/sobre",function(req, res){
-    res.send("Minha pag sobre")
+app.get("/sobre", function (req, res) {
+  res.send("Minha pag sobre");
 });
 
-app.get("/blog", function(req, res){
-    res.send("Bem vindo ao blog")
+app.get("/blog", function (req, res) {
+  res.send("Bem vindo ao blog");
 });
 
 //Parâmetros
-app.get("/ola/:nome/:cargo/:cor",function(req, res){
-    res.send("Ola "+req.params.nome+"Sua cor favorita é: "+req.params.cor+"Seu cargo é: "+req.params.cargo)
+app.get("/ola/:nome/:cargo/:cor", function (req, res) {
+  res.send(
+    "Ola " +
+      req.params.nome +
+      "Sua cor favorita é: " +
+      req.params.cor +
+      "Seu cargo é: " +
+      req.params.cargo
+  );
 });
 
-
-app.listen(8980,function(){
-    console.log("Servidor rodando na porta 8981");
+app.listen(8980, function () {
+  console.log("Servidor rodando na porta 8981");
 });
